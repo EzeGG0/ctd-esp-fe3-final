fa7de6bcd3707e0cca5497a2abc9e4d3bce73574
Eze <94138154+EzeGG0@users.noreply.github.com>
2024-03-29 18:47:06 -0300

    changes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -13,7 +13,7 @@ const Card = ({ name, username, id }) => {
         {/* En cada card deberan mostrar en name - username y el id */}
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
         <div className="card">
-          <img src="/public/images/doctor.jpg"/>
+          <img src="public/images/doctor.jpg"/>
           <h2>{name}</h2>
           <p>{username}</p>
           <Link to={`/details/${id}`}>Ver detalles</Link>
